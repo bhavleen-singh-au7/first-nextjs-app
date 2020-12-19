bc376219fae6e8265802cd4306270fa6736838b1
Bhavleen Singh <singhbhavleen3@gmail.com>
2020-12-19 16:04:21 +0530

    Adding cs and api request

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,34 @@
 import Link from "next/link";
-import Hello from "./api/hello";
 
-const Home = () => {
+export async function getStaticProps() {
+  const res = await fetch(
+    "http://localhost:3000/api/hello"
+  );
+  const data = await res.json();
+  console.log(data);
+  return {
+    props: { data },
+  };
+}
+
+const Home = (prop) => {
   return (
     <div
       style={{ backgroundColor: "red", padding: "50px" }}>
       <h1>Welcome To Home Page</h1>
-      
+      <h3>{prop.data.message}</h3>
       <Link href="/product">Product Section</Link>
       <br />
       <Link href="/profile">Profile Section</Link>
+
+      {/* Adding CSS */}
+      <style jsx>
+        {`
+          h3 {
+            color: white;
+          }
+        `}
+      </style>
     </div>
   );
 };
