be33f6d97a1b9741408a792a3aad75f01f0848f5
Bhavleen <singhbhavleen3@gmail.com>
2020-12-24 07:08:57 +0530

    Test Added

--- pages/index.js
+++ pages/index.js
@@ -17,6 +17,7 @@ const Home = (prop) => {
       style={{ backgroundColor: "red", padding: "50px" }}>
       <h1>Welcome To Home Page</h1>
       {/* <h3>{prop.data.message}</h3> */}
+      <h6>Test</h6>
       <Link href="/product">Product Section</Link>
       <br />
       <Link href="/profile">Profile Section</Link>
